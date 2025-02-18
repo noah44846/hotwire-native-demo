5c1e96a2c1c6b1fd3f7a93b79d6059612112e66c
Joe Masilotti <joe@masilotti.com>
2025-02-18 09:29:22 -0800

    Use ActiveModel for error flash messages

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -3,17 +3,29 @@ class ResourcesController < ApplicationController
   end
 
   def new
+    @resource = Resource.new
   end
 
   def create
-    redirect_to resource_path(
-      first_name: params[:first_name],
-      last_name: params[:last_name]
-    ), notice: "Form submitted successfully."
+    @resource = Resource.new(resource_params)
+    if @resource.valid?
+      redirect_to resource_path(
+        first_name: @resource.first_name,
+        last_name: @resource.last_name
+      ), notice: "Form submitted successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
     @first_name = params[:first_name]
     @last_name = params[:last_name]
+  end
+
+  private
+
+  def resource_params
+    params.require(:resource).permit(:first_name, :last_name)
   end
 end
