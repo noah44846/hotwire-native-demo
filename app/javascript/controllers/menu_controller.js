da475675c89298793871884c88ade32297e0a124
Adrien Maston <adrien@37signals.com>
2025-02-28 20:04:25 +0100

    Remove Bootstrap and re-style

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,18 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["dialog", "item", "result"]
+  static targets = ["dialog", "dialogContent", "item", "result"]
 
   show() {
-    this.dialogTarget.style.display = "block"
+    this.dialogTarget.showModal()
   }
 
   hide() {
-    this.dialogTarget.style.display = "none"
+    this.dialogTarget.close()
   }
 
   hideOnClickOutside({ target }) {
-    if (this.dialogTarget == target) {
+    if (this.dialogTarget.contains(target) && !this.dialogContentTarget.contains(target)) {
       this.hide()
     }
   }
