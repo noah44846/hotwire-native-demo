6562a57c4f66344df32fe1aaedd8db24a02019b4
Jay Ohms <jay@37signals.com>
2025-04-21 17:13:23 -0400

    Add the android remote path configuration

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
   resources :configurations, only: [] do
     get "ios_v1", on: :collection
+    get "android_v1", on: :collection
   end
 
   resource :dashboards, only: :show
